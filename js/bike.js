b3d99449a6b1dfa1be4e7b324cb9aa0aa5efde7e
momajd <mo.a.majd@gmail.com>
2016-05-09 21:51:58 -0700

    board

--- js/bike.js
+++ js/bike.js
@@ -84,5 +84,27 @@ Board.BLANK_SYMBOL = ".";
 Board.blankGrid = function (dim) {
   var grid = [];
 
-  
-}
+  for (var i = 0; i < dim; i++) {
+    var row = [];
+    for (var j = 0; j < dim; j++) {
+      row.push(Board.BLANK_SYMBOL);
+    }
+    grid.push(row);
+  }
+  grid.push(row);
+};
+
+Board.prototype.render = function () {
+  var grid = Board.blankGrid(this.dim);
+
+  this.bike.segments.forEach(function (segment) {
+    grid[segment.i][segment.j] = Bike.SYMBOL;
+  });
+
+  // join the grid into a big string
+  grid.map(function (row) {
+    return row.join("");
+  }).join("\n");
+};
+
+module.exports = Board;
